217efbf6330234b3feca1c88085ac486fe6eaa86
Russell <russhwolf@gmail.com>
2019-09-08 13:06:16 -0400

    Display location labels in list and delete on click

--- soluna-mobile/shared/xcodeproj/Soluna/LocationListViewController.swift
+++ soluna-mobile/shared/xcodeproj/Soluna/LocationListViewController.swift
@@ -9,23 +9,53 @@
 import UIKit
 import Shared
 
-class LocationListViewController: UITableViewController, UINavigationControllerDelegate {
+class LocationListViewController: UIViewController, UINavigationControllerDelegate, UITableViewDelegate, UITableViewDataSource {
     
     let viewModel = LocationListViewModel(repository: SwiftKotlinBridgeKt.repository, dispatcher: MainThreadKt.mainDispatcher)
+
+    private var items: Array<LocationSummary> = []
+    
+    @IBOutlet var locationList: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
         
+        locationList.register(UINib(nibName: "LocationListItem", bundle: nil), forCellReuseIdentifier: "LocationListItem")
+
         viewModel.setViewStateListener { (state: LocationListViewState) in
-            let locations = state.locations
-            NSLog(String(locations.count))
+            self.setItems(items: state.locations)
         }
+    }
+    
+    private func setItems(items: [LocationSummary]) {
+        self.items = items
+        self.locationList.reloadData()
     }
     
     @IBAction func onAddLocationClick(_ sender: Any) {
         let addLocationViewController = self.storyboard?.instantiateViewController(withIdentifier: "AddLocation")
         navigationController?.pushViewController(addLocationViewController!, animated: true)
     }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return items.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "LocationListItem", for: indexPath) as! LocationListItem
+        let index = indexPath.row
+        
+        cell.label.text = items[index].label
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let index = indexPath.row
+        viewModel.removeLocation(id: items[index].id)
+    }
 }
 
+class LocationListItem: UITableViewCell {
+    @IBOutlet var label: UILabel!
+}
