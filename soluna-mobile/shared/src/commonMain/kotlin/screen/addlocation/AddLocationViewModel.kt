e92f87db1748224f88f49fcaf41d80a3e0fd8ecf
Russell <russhwolf@gmail.com>
2019-09-12 22:52:46 -0400

    Error-handling and other updates in AddLocationViewModel

--- soluna-mobile/shared/src/commonMain/kotlin/screen/addlocation/AddLocationViewModel.kt
+++ soluna-mobile/shared/src/commonMain/kotlin/screen/addlocation/AddLocationViewModel.kt
@@ -7,28 +7,15 @@ import com.russhwolf.soluna.mobile.util.EventTrigger
 import com.russhwolf.soluna.mobile.util.mainDispatcher
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Job
-import kotlinx.coroutines.async
 
 class AddLocationViewModel(private val repository: SolunaRepository, dispatcher: CoroutineDispatcher = mainDispatcher) :
     BaseViewModel<AddLocationViewState>(AddLocationViewState(), dispatcher) {
-    fun addLocation(label: String, latitude: String, longitude: String, timeZone: String): Job {
-        val latitudeDouble = latitude.toDoubleOrNull()
-        val longitudeDouble = longitude.toDoubleOrNull()
+    fun addLocation(label: String, latitude: String, longitude: String, timeZone: String): Job = updateAsync {
+        val latitudeDouble = latitude.toDouble()
+        val longitudeDouble = longitude.toDouble()
 
-        if (latitudeDouble == null || longitudeDouble == null) {
-            // TODO error UI
-            // (just no-op for now)
-            return coroutineScope.async { Unit }
-        }
-
-        // TODO: passing a value with the name "label" causes a compiler error in native on 1.3.4x,
-        //  but appears fixed in 1.3.50 eap
-        @Suppress("UnnecessaryVariable")
-        val renamed = label
-        return updateAsync {
-            repository.addLocation(renamed, latitudeDouble, longitudeDouble, timeZone)
-            state.copy(exitTrigger = EventTrigger.create())
-        }
+        repository.addLocation(label, latitudeDouble, longitudeDouble, timeZone)
+        state.copy(exitTrigger = EventTrigger.create())
     }
 
     fun geocodeLocation(location: String) = updateAsync {
