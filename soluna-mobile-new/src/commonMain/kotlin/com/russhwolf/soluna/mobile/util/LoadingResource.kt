855043a6e82ad3572a5b40caa680dd3d3fbd79d0
russell <russhwolf@gmail.com>
2025-02-18 20:58:06 -0500

    Add bad h4x to clean up jumpiness for now

--- soluna-mobile-new/src/commonMain/kotlin/com/russhwolf/soluna/mobile/util/LoadingResource.kt
+++ soluna-mobile-new/src/commonMain/kotlin/com/russhwolf/soluna/mobile/util/LoadingResource.kt
@@ -4,6 +4,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.ProduceStateScope
 import androidx.compose.runtime.State
 import androidx.compose.runtime.produceState
+import kotlinx.coroutines.delay
 
 sealed interface LoadingResource<out T> {
     data object Loading : LoadingResource<Nothing>
@@ -25,6 +26,8 @@ fun <T> produceLoadingResourceState(
     producer: suspend ProduceStateScope<LoadingResource<T>>.() -> Unit
 ): State<LoadingResource<T>> {
     return produceState<LoadingResource<T>>(LoadingResource.Loading, key) {
+        value = LoadingResource.Loading // Without this, we don't reset to loading when key changes
+        delay(50) // Give us a sec to settle (TODO h4x!)
         producer.invoke(this)
     }
 }
